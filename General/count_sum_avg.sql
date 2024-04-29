331299f38fa82d6bd835435bda26fd092b5f27a5
mailongvy <mlongvy0708@gmail.com>
2024-04-29 22:38:45 +0700

    latest update

---
+++ General/count_sum_avg.sql
@@ -0,0 +1,49 @@
+﻿-- các hàm count sum avg
+
+--count: đếm số lượng bản ghi khác null
+-- select Count(column_name)
+-- from table_name
+
+-- sum: tính tổng giá trị của một cột nếu bất kì giá trị trong cột là null thì kết quả cảu hàm là null
+--select Sum(column_name)
+-- from table_name
+
+--avg: tính giá trị trung bình cho một cột
+-- nếu tất cả các giá trị trong cột là null kết quá của hàm avg sẽ là null 
+-- nếu có vài cột có giá trị là null thì avg sẽ bỏ qua các giá trị null và tính giá trị trung bình cho các giá trị khác
+
+-- hãy đếm số lượng khách hàng có trong bảng customers 
+select COUNT(*) as "NumbersOfCustomers"
+from Customers as c;
+
+--tổng số tiền vận chuyển (Freight) của tất cả các đơn hàng 
+select SUM(o.Freight) as "Tổng số tiền vận chuyển (Freight)"
+from Orders as o;
+
+-- tính trung bình số lượng đặt hàng (Quantity) của tất cả các sản phẩm trong bảng order details
+select AVG(o.Quantity) as "Trung bình số lượng đặt hàng"
+from [Order Details] as "o"
+
+-- đếm số lượng, tính tổng số lượng hàng tồn kho và trung bình giá các sản phẩm có trong bảng product
+-- bản chất của hàng tồn kho là lấy số lượng hàng có trong kho - số lượng hàng đã đc order 
+-- unitsíntock - unitsorder
+select (COUNT(p.UnitsInStock) - COUNT(p.UnitsOnOrder)) as "Số lượng hàng tồn kho", 
+	   (SUM(p.UnitsInStock) - SUM(UnitsOnOrder)) as "Tổng số lượng của hàng tồn kho",
+	   AVG(p.UnitPrice) as "Giá trị trung bình của các sản phẩm có trong kho"
+from Products as "p";
+
+-- BTAP 
+-- hãy đếm số lượng đơn hàng từ order = 2 cách
+--c1: dùng *
+select COUNT(*) as "Số lượng đơn hàng"
+from Orders as "o";
+--c2: dùng mã đơn hàng
+select COUNT(o.OrderID) as "Số lượng đơn hàng 2"
+from Orders as "o";
+
+
+--BTAP 2
+-- từ bảng order details hãy tính trung bình cho cột unitprice và tính tổng cho cột quantity
+select AVG(OD.UnitPrice) as "Giá trị trung bình trên một đơn vị sản phẩm",
+	   SUM(OD.Quantity) as "Tổng số lượng của tất cả các sản phẩm"
+from [Order Details] as "OD"
---
+++ General/max_min.sql
@@ -0,0 +1,38 @@
+﻿-- max min trong sql
+
+-- tìm giá trị nhỏ nhất
+-- select MIN(column_name)
+-- from table_name; có thể kết hợp alias vào để thay đổi tên cột vào 
+
+-- tìm giá trị lớn nhất
+-- select MAX(column_name)
+-- from table_name; có thể kết hợp alias vào để thay đổi tên cột vào 
+
+-- viết câu lệnh sql tìm giá thấp nhất của sản phẩm trong bảng product
+select MIN(UnitPrice) as "giá tiền thấp nhất"
+from Products as p;
+
+-- viết câu lệnh lấy ngày đặt hàng gần đây nhất từ bảng order
+select MAX(o.OrderDate)
+from Orders as o;
+
+-- viết câu lệnh sql tìm ra sản phẩm (lấy mã và tên sản phẩm) có số lượng hàng tốt kho (unitsInstock)
+select MAX(p.UnitsInStock) as MaxUnitsInStock
+from Products as p;
+
+
+-- Viết câu lệnh cho bik tuổi đời nhân viên lớn nhất của công ty
+-- hint: ai có ngày sinh càng nhỏ thì người đó cảng lớn tuổi
+select MIN(e.BirthDate) as "Người có tuổi đời lớn nhất"
+from Employees as e;
+
+--Thử thách:
+--TT1: Viết câu lệnh SQL tìm ra số lượng sản phẩm nhỏ nhất trên mỗi đơn vị của các sản phẩm trong bảng Products
+select Min(p.QuantityPerUnit) as "Số lượng sản phẩm nhỏ nhất trên mỗi đơn vị"
+from Products as p;
+--TT2: Viết câu lệnh SQL cho biết địa chỉ gần nhất và quốc gia lớn nhất của các nhà cung cấp trong bảng Suppliers
+-- địa chỉ gần nhất
+select MIN(s.Address) as "địa chỉ gần nhất", MIN(s.Country) as "quốc gia lớn nhất"
+from Suppliers as s;
+
+
