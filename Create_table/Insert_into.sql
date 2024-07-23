9d9ec2cc7a1c99e5de5e83e72e473503acf83e7a
mailongvy <mlongvy0708@gmail.com>
2024-07-23 14:47:37 +0700

    latest update 23/7/2024

--- Create_table/Insert_into.sql
+++ Create_table/Insert_into.sql
@@ -8,5 +8,36 @@
 select *
 from Customers as c;
 
+select *
+from Employees as e;
+
+select *
+from Shippers as s;
+
+-- thêm một khách hàng
 Insert into Customers (CustomerID, CompanyName, ContactName, Phone)
 values ('KH123', 'MLV.VN', 'Mai Long Vy', '0123456789');
+
+-- them một khách hàng với tất cả các cột
+Insert into Customers 
+values ('KH456', 'MLV.VN', 'Mai Long Vy', '0123456789', '-', null, null, null, null, null, null);
+
+-- thêm nhiều khách hàng mới cùng lúc
+Insert into Customers (CustomerID, CompanyName, ContactName, Phone)
+values 
+('KH124', 'MLV.VN', 'Mai Long Vy', '0123456789'),
+('KH125', 'MLV.VN', 'Mai Long Vy', '0123456789'),
+('KH126', 'MLV.VN', 'Mai Long Vy', '0123456789'),
+('KH127', 'MLV.VN', 'Mai Long Vy', '0123456789');
+
+-- thêm một sản phẩm mới vào bảng products
+Insert into Products(ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock) 
+values ('New Product', 1, 2, '24 bottles', 10.99, 100);
+
+-- viết câu lệnh để thêm nhà cung cấp vào bảng suppliers
+insert into Suppliers(CompanyName, [ContactName], [ContactTitle], [Address], [City], [Region], [PostalCode], [Country], [Phone], Fax, HomePage)
+values
+('New Supplier', 'John Smith', 'Sale Manager', '123 Supplier Street', 'New York', 'NY', '10001', 'USA', '555-555-5555', '555-555-5556', 'http://www.newsupplier.com');
+
+insert into Orders(CustomerID, EmployeeID, OrderDate, ShipVia) 
+values ('KH123', 1, GETDATE(), 2);
